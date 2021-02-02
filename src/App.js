6e2eeb91bc2c4e313fb5648a8b42fce0acf87fd9
edwardCwh11039 <51016142+edwardCwh11039@users.noreply.github.com>
2021-02-02 11:12:29 +1100

    update website

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Home from "./components/pages/Home";
+import Services from "./components/pages/Services.js";
+import Projects from "./components/pages/Projects";
+import SignUp from "./components/pages/SignUp";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/services" component={Services} />
+          <Route path="/projects" component={Projects} />
+          <Route path="/sign-up" component={SignUp} />
+        </Switch>
+      </Router>
     </div>
   );
 }
